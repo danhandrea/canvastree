15c86dff8e000045d3ad1f9224f6cb12f48cf4b7
Dan Handrea <handrea.dan@gmail.com>
2017-06-28 19:36:35 +0100

    17

--- src/Canvas/index.js
+++ src/Canvas/index.js
@@ -1,9 +1,13 @@
 export default class Canvas {
     html(id) {
-        if (!id || !/^[A-Za-z]+[\w\-\:\.]*$/.test(id)) throw new Error(`Invalid id specified: ${id} `);
+        if (!id || !/^[A-Za-z]+[\w\-]*$/.test(id)) throw new Error(`Invalid id specified: ${id}. Default rules, minus '.' and '-'.`);
 
         let canvas = document.createElement('canvas');
+        
         canvas.id = id;
+
+        canvas.width = document.body.clientWidth;
+        canvas.height = document.body.clientHeight;
 
         return canvas;
     }
