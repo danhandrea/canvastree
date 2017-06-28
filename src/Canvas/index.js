29b2c4b16320fb4eb3940b3e05ae8203d40fc386
Dan Handrea <handrea.dan@gmail.com>
2017-06-28 19:26:45 +0100

    13

--- src/Canvas/index.js
+++ src/Canvas/index.js
@@ -1,6 +1,5 @@
-/*@flow*/
 export default class Canvas {
-    html(id: string) {
+    html(id) {
         if (!id || /^[A-Za-z]+[\w\-\:\.]*$/.test(id)) throw new Error(`Invalid id specified: ${id} `);
 
         let canvas = document.createElement('canvas');
